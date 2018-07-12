f7eb41307eb11c4ad9b2e3204b7eb9c8372fa94b
Viacheslav Dushin <slava@dushin.pro>
2018-07-12 19:22:30 +0300

    setting up the package

---
+++ README.md
@@ -0,0 +1,5 @@
+# Description
+CLEAN algorithm implementation
+# References
+http://adsabs.harvard.edu/abs/1987AJ.....93..968R
+www.astro.spbu.ru/sites/default/files/uneven.pdf
--- setup.py
+++ setup.py
@@ -2,12 +2,22 @@
 from setuptools import setup, find_packages
 from os import path
 
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='pyclean',
     version='0.0.1',
+    #TBD: add references
     description='Python CLEAN implementation',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/pypa/sampleproject'
     classifiers=[
+        #TODO: test with other versions
         'Programming Language :: Python :: 3.6',
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License'
     ],
     install_requires=[
        'numpy>=1.14.0<1.15.0'
