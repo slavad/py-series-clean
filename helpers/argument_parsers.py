3eb4f511e1ae78be12df513c6e4c6cde738c9493
Viacheslav Dushin <slava@dushin.pro>
2018-07-17 22:28:39 +0300

    renaming options

--- helpers/argument_parsers.py
+++ helpers/argument_parsers.py
@@ -2,7 +2,7 @@ import helpers.input_validators as iv
 def parse_common_series_args(parser):
     """these args are common for estimate_treshold and py_series_clean"""
     parser.add_argument(
-        '-f',
+        '-i',
         required = True,
         help='input file location, input file should be a text file of two columns: "time value"',
         metavar='INPUT'
