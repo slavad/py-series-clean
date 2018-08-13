1d7bae205d66125f41ea8b5e6841d6d8e9c89805
Viacheslav Dushin <slava@dushin.pro>
2018-08-13 15:59:54 +0300

    converting threshold estimator to a class

--- py_series_clean/threshold.py
+++ py_series_clean/threshold.py
@@ -2,46 +2,50 @@ import numpy as np
 import py_series_clean.matrix_builder as mb
 import py_series_clean.schuster as sch
 
-def generate_random_series(time_grid_len, number_of_series, sigma):
-    """generates an array of random normal values (time_grid_len,number_of_series)"""
-    result = np.random.normal(loc=0.0, scale=sigma, size=(time_grid_len, number_of_series))
-    return result
-
-def generate_index_vector(vector_size):
-    """generates index vector: from 0 to vector_size"""
-    index_vector = np.arange(0, vector_size + 1, 1).reshape(-1,1)
-    return index_vector
-
-def find_max_counts_and_relation(random_series, values):
-    """
-        Finds the number of maximun counts in the
-        Schuster periodorgrams for the random series
-        that are > than the average
-        count in the Schuster periodorgram for the examined observation,
-        then calculates the relation of the number above
-        to the total number of the random series.
-    """
-    number_of_random_series = random_series.shape[1]
-    max_counts_random = sch.calc_schuster_counts(random_series, method_flag='max')
-    average_count_series = sch.calc_schuster_counts(values, method_flag='average')[0]
-    # extract ids that fit to our condition
-    ids_list = np.where(max_counts_random >= average_count_series)
-    count_of_items = ids_list[0].shape[0]
-    # the probabilty that the examined series contain noise only:
-    relation = count_of_items/number_of_random_series
-    # the probabilty that the examined series contain also signal:
-    return 1 - relation
-
-def estimate_threshold(time_grid_and_values, number_of_random_series, sigma, khi):
+class Threshold(object):
     """estimate treshold for the given params"""
-    time_grid = time_grid_and_values[0]
-    values = time_grid_and_values[1]
-    max_freq = mb.estimate_max_freq(time_grid)
-    number_of_freq_estimations = mb.calculate_estimations_vector_size(
-        max_freq, time_grid, khi
-    )
-    random_series = generate_random_series(time_grid.shape[0], number_of_random_series, sigma)
-    threshold = find_max_counts_and_relation(
-        random_series, values
-    )
-    return threshold
+
+    def __init__(self, time_grid_and_values, sigma, khi):
+        """estimate treshold for the given params"""
+        self.__time_grid = time_grid_and_values[0]
+        self.__values = time_grid_and_values[1]
+        self.__sigma = sigma
+        self.__khi = khi
+        self.__max_freq = mb.estimate_max_freq(self.__time_grid)
+        self.__number_of_freq_estimations = mb.calculate_estimations_vector_size(
+            self.__max_freq, self.__time_grid, self.__khi
+        )
+
+    def estimate(self, number_of_random_series):
+        """estimates the treshold"""
+        random_series = self.__generate_random_series(self.__time_grid.shape[0])
+        threshold = self.__find_max_counts_and_relation(
+            random_series, self.__values
+        )
+        return threshold
+
+    def __generate_random_series(self, number_of_random_series):
+        """generates an array of random normal values (time_grid_len,number_of_series)"""
+        time_grid_len = self.__time_grid.shape[0]
+        result = np.random.normal(loc=0.0, scale=self.__sigma, size=(time_grid_len, number_of_random_series))
+        return result
+
+    def __find_max_counts_and_relation(self, random_series, values):
+        """
+            Finds the number of maximun counts in the
+            Schuster periodorgrams for the random series
+            that are > than the average
+            count in the Schuster periodorgram for the examined observation,
+            then calculates the relation of the number above
+            to the total number of the random series.
+        """
+        number_of_random_series = random_series.shape[1]
+        max_counts_random = sch.calc_schuster_counts(random_series, method_flag='max')
+        average_count_series = sch.calc_schuster_counts(values, method_flag='average')[0]
+        # extract ids that fit to our condition
+        ids_list = np.where(max_counts_random >= average_count_series)
+        count_of_items = ids_list[0].shape[0]
+        # the probabilty that the examined series contain noise only:
+        relation = count_of_items/number_of_random_series
+        # the probabilty that the examined series contain also signal:
+        return 1 - relation
