d99ffc7b3b0fb667db4d9fe7d4596d1c4b43e3de
Viacheslav Dushin <slava@dushin.pro>
2018-08-16 15:45:10 +0300

    first spec

--- spec/generator/dirty_series_spec.py
+++ spec/generator/dirty_series_spec.py
@@ -1,8 +1,34 @@
-from mamba import description, context, it
-from expects import expect, equal
+from spec.spec_helper import *
 import generator.dirty_series as ds
 
 with description(ds.DirtySeries) as self:
+    with before.all:
+      self.time_grid_length = 10
+      self.max_time_value = 20
+      self.frequencies = np.array([0.1, 0.2])
+      self.phases = np.array([0.4, 0.6])
+      self.amplitudes = np.array([10, 20])
+      self.sigma = 1
+    with before.each:
+        self.generator = ds.DirtySeries(
+            self.time_grid_length, self.max_time_value,
+            self.frequencies, self.phases,
+            self.amplitudes, self.sigma
+        )
     with description('DirtySeries#__reshape_one_value'):
         with it('converts scalar to array'):
-            expect(1).to(equal(1))
+            expect(
+                self.generator._DirtySeries__reshape_one_value(1)
+            ).to(
+                equal(np.array([[1]]))
+            )
+        with it('reshapes 1d array'):
+            expect(
+                np.all(
+                    self.generator._DirtySeries__reshape_one_value(
+                        np.array([1,2])
+                    ) == np.array([[1],[2]])
+                )
+            ).to(
+                equal(True)
+            )
---
+++ spec/spec_helper.py
@@ -0,0 +1,4 @@
+import numpy as np
+from mamba import description, context, it
+from expects import expect, equal
+import pdb
