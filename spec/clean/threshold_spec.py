aec6cef80cb5022801bb3761841fbd57642a53d9
Viacheslav Dushin <slava@dushin.pro>
2018-08-23 16:46:03 +0300

    adding more specs

--- spec/clean/threshold_spec.py
+++ spec/clean/threshold_spec.py
@@ -9,6 +9,7 @@ with description(thrs.Threshold) as self:
         )
         self.sigma = 1
         self.khi = 4
+        self.number_of_random_series = 1000
     with before.each:
         self.estimator = thrs.Threshold(self.time_grid_and_values, self.sigma, self.khi, self.use_aver)
     with shared_context('object values setter'):
@@ -34,6 +35,28 @@ with description(thrs.Threshold) as self:
             ).to(
                 equal(self.estimator._Threshold__sigma)
             )
+    with shared_context('random series generator'):
+        with before.each:
+            self.random_series_array = self.estimator._Threshold__generate_random_series(
+                self.number_of_random_series
+            )
+        with it('generates array with correct shape'):
+            expected_shape = (self.time_grid_and_values[0].shape[0], self.number_of_random_series)
+            expect(self.random_series_array.shape).to(
+                equal(expected_shape)
+            )
+        with it('series are always different'):
+            new_random_series_array = self.estimator._Threshold__generate_random_series(
+                self.number_of_random_series
+            )
+            expect(
+                np.all(
+                    new_random_series_array == self.random_series_array
+                )
+
+            ).to(
+                equal(False)
+            )
     with description('use_aver is False'):
         with before.all:
             self.use_aver = False
@@ -47,6 +70,11 @@ with description(thrs.Threshold) as self:
                 )
             with included_context('object values setter'):
                 pass
+
+        with description('#__generate_random_series'):
+            with included_context('random series generator'):
+                pass
+
     with description('use aver is True'):
         with before.all:
             self.use_aver = True
@@ -59,4 +87,7 @@ with description(thrs.Threshold) as self:
                     equal(self.estimator._Threshold__number_of_freq_estimations)
                 )
             with included_context('object values setter'):
+                pass
+        with description('#__generate_random_series'):
+            with included_context('random series generator'):
                 pass
