614c697f8af33bd7fce6f70821a00c16c3006ac8
Viacheslav Dushin <slava@dushin.pro>
2018-08-23 18:34:12 +0300

    adding dummy specs

---
+++ spec/clean/iterator_spec.py
@@ -0,0 +1,8 @@
+from spec.spec_helper import *
+import clean.iterator as itr
+
+with description(itr.Iterator) as self:
+    with description('__init__'):
+        #TODO: test helper methods used here
+        with it('sets all values'):
+            pass
---
+++ spec/clean/schuster_spec.py
@@ -0,0 +1,8 @@
+from spec.spec_helper import *
+import clean.schuster as sch
+
+with description(sch) as self:
+    with description('#calc_schuster_counts'):
+        pass
+    with description('#squared_abs'):
+        pass
