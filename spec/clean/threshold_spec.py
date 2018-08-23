b852ee0ebdced49069caa44ad88149e977557b62
Viacheslav Dushin <slava@dushin.pro>
2018-08-23 16:21:56 +0300

    adding more specs

--- spec/clean/threshold_spec.py
+++ spec/clean/threshold_spec.py
@@ -2,7 +2,59 @@ from spec.spec_helper import *
 import clean.threshold as thrs
 
 with description(thrs.Threshold) as self:
-    with description('#__init__'):
+    with before.all:
+        self.time_grid_and_values = (
+            np.load("./spec/fixtures/time_grid_1.pickle"),
+            np.load("./spec/fixtures/series_1.pickle")
+        )
+        self.sigma = 1
+        self.khi = 4
+    with before.each:
+        self.estimator = thrs.Threshold(self.time_grid_and_values, self.sigma, self.khi, self.use_aver)
+    with shared_context('object values setter'):
         with it('sets all values'):
-            #TODO also spec matrix builder methods used here
-            pass
+            expect(
+                np.all(
+                    self.time_grid_and_values[0] == self.estimator._Threshold__time_grid
+                )
+
+            ).to(
+                equal(True)
+            )
+            expect(
+                np.all(
+                    self.time_grid_and_values[1] == self.estimator._Threshold__values
+                )
+
+            ).to(
+                equal(True)
+            )
+            expect(
+                self.sigma
+            ).to(
+                equal(self.estimator._Threshold__sigma)
+            )
+    with description('use_aver is False'):
+        with before.all:
+            self.use_aver = False
+        with description('#__init__'):
+            with it('sets number_of_freq_estimations'):
+                expect(
+                    35934
+                ).to(
+                    equal(self.estimator._Threshold__number_of_freq_estimations)
+                )
+            with included_context('object values setter'):
+                pass
+    with description('use aver is True'):
+        with before.all:
+            self.use_aver = True
+        with description('#__init__'):
+            with it('sets number_of_freq_estimations'):
+                expect(
+                    198
+                ).to(
+                    equal(self.estimator._Threshold__number_of_freq_estimations)
+                )
+            with included_context('object values setter'):
+                pass
