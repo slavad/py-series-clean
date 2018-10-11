cd68a9a2ed23273ea366fd665d44d2534c59233e
Viacheslav Dushin <slava@dushin.pro>
2018-10-12 01:54:09 +0300

    refactoring

--- clean/wrapper.py
+++ clean/wrapper.py
@@ -8,26 +8,25 @@ class Wrapper(object):
     """
         wraps Iterator, Restorer and DetectionTreshold classes
     """
-    def __init__(self, time_grid, khi, use_aver):
+    def __init__(self, time_grid):
         self.__time_grid = time_grid
-        self.__use_aver = use_aver
-        self.__max_freq = mb.estimate_max_freq(self.__time_grid, self.__use_aver)
-        self.__number_of_freq_estimations = mb.calculate_estimations_vector_size(
-            self.__max_freq, self.__time_grid, khi
-        )
 
-    def clean(self, values, detection_treshold, max_iterations, harmonic_share):
+    def clean(self, values, detection_treshold, max_iterations, harmonic_share, khi, use_aver):
         """restores clean time series and spectrum from durty one"""
+        max_freq = mb.estimate_max_freq(self.__time_grid, use_aver)
+        number_of_freq_estimations = mb.calculate_estimations_vector_size(
+            max_freq, self.__time_grid, khi
+        )
         iterator = itr.Iterator(
             detection_treshold,
-            harmonic_share, self.__number_of_freq_estimations,
-            self.__time_grid, values, self.__max_freq
+            harmonic_share, number_of_freq_estimations,
+            self.__time_grid, values, max_freq
         )
 
         iterations_result = iterator.iterate(max_iterations)
         restorer = rst.Restorer(
             iterations_result['super_resultion_vector'], iterations_result['iterations'],
-            self.__number_of_freq_estimations, self.__time_grid, self.__max_freq
+            number_of_freq_estimations, self.__time_grid, max_freq
         )
 
         restoration_result = restorer.restore()
